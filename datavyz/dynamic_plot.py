6ddddd24cb34eb34c03f27fcf8fd0dd7e00cba12
Yann <yann.zerlaut@icm-institute.org>
2020-03-18 17:11:57 +0100

    update origin convention in movies

--- datavyz/dynamic_plot.py
+++ datavyz/dynamic_plot.py
@@ -9,7 +9,9 @@ def animated_plot(x,
                   time_dep_y,
                   graph_env,
                   time=None,
-                  annotation={'text':'t=%is','xy':(.5, .6), 'color':'r'},
+                  annotation_text='t=%is',
+                  annotation_xy=(.5, .6),
+                  annotation_args={'color':'r', 'weight':'bold'},
                   interval=400,
                   color=None,
                   lw=1, ms=0., marker='o',
@@ -24,20 +26,19 @@ def animated_plot(x,
         color = graph_env.default_color
         
     line, = ax.plot(x, time_dep_y[0,:], lw=lw, color=color, ms=ms, marker=marker)
-    title = ax.annotate(annotation['text'] % time[0],
-                        annotation['xy'],
-                        color=annotation['color'],
-                        xycoords='axes fraction', ha='center')
+    title = ax.annotate(annotation_text % time[0],
+                        annotation_xy,
+                        **annotation_args)
     graph_env.set_plot(ax, ylim=[time_dep_y.min(), time_dep_y.max()], **axes_args)
 
     # Init only required for blitting to give a clean slate.
     def init():
-        title.set_text(annotation['text'] % time[0])
+        title.set_text(annotation_text % time[0])
         line.set_ydata(np.ma.array(x, mask=True))
         return [line,title]
     
     def animate(i):
-        title.set_text(annotation['text'] % time[i])
+        title.set_text(annotation_text % time[i])
         line.set_ydata(time_dep_y[i,:])
         return [line,title]
 
@@ -55,9 +56,12 @@ def animated_plot(x,
 def movie_plot(array,
                graph_env,
                time=None,
-               annotation={'text':'t=%is','xy':(.5, .6), 'color':'r'},
+               annotation_text='t=%is',
+               annotation_xy=(.5, .6),
+               annotation_args={'color':'r', 'weight':'bold'},
                interval=400,
                cmap=None,
+               aspect='equal',
                axes_args={},
                fig_args={}):
 
@@ -69,27 +73,25 @@ def movie_plot(array,
     if cmap is None:
         cmap = graph_env.binary
         
-    im = ax.imshow(array[0,:,:], cmap=cmap,
+    im = ax.imshow(array[0,:,:].T, cmap=cmap,
                    interpolation=None,
                    origin='lower',                
-                   aspect='equal')
+                   aspect=aspect)
     
-    title = ax.annotate(annotation['text'] % time[0],
-                        annotation['xy'],
-                        color=annotation['color'],
-                        xycoords='axes fraction', ha='center')
+    title = ax.annotate(annotation_text % time[0],
+                        annotation_xy,
+                        **annotation_args)
 
     # Init only required for blitting to give a clean slate.
     def init():
-        title.set_text(annotation['text'] % time[0])
-        im.set_array(array[0,:,:])
+        title.set_text(annotation_text % time[0])
+        im.set_array(array[0,:,:].T)
         return [im,title]
     
     def animate(i):
-        title.set_text(annotation['text'] % time[i])
-        im.set_array(array[i,:,:])
+        title.set_text(annotation_text % time[i])
+        im.set_array(array[i,:,:].T)
         return [im,title]
-
 
     ani = animation.FuncAnimation(fig,
                                   animate,
@@ -109,23 +111,23 @@ if __name__=='__main__':
     from datavyz.main import graph_env
     ge = graph_env('manuscript')
     
-    # fig, ax, ani = animated_plot(np.arange(20),
-    #                              np.random.randn(100, 20),
-    #                              ge,
-    #                              time = np.linspace(0, 1, 100),
-    #                              annotation={'text':'t=%.1fs', 'xy':(.2,.7), 'color':ge.red},
-    #                              axes_args={'xlabel':'xlabel (xunit)',
-    #                                         'ylabel':'ylabel (yunit)'})
+    fig, ax, ani = animated_plot(np.arange(20),
+                                 np.random.randn(100, 20),
+                                 ge,
+                                 time = np.linspace(0, 1, 100),
+                                 axes_args={'xlabel':'xlabel (xunit)',
+                                            'ylabel':'ylabel (yunit)'})
 
-    # fig, ax, ani = movie_plot(np.random.randn(100, 50, 30),
-    #                           ge,
-    #                           time = np.linspace(0, 1, 100),
-    #                           annotation={'text':'t=%.1fs', 'xy':(.2,.7), 'color':ge.red},
-    #                           axes_args={'xlabel':'xlabel (xunit)',
-    #                                      'ylabel':'ylabel (yunit)'})
+    fig, ax, ani = movie_plot(np.random.randn(100, 16, 9),
+                              ge,
+                              time = np.linspace(0, 1, 100),
+                              annotation_text='t=%.1fs',
+                              annotation_args={'color':ge.red, 'weight':'bold'},
+                              axes_args={'xlabel':'xlabel (xunit)',
+                                         'ylabel':'ylabel (yunit)'})
 
     # fig, ax = ge.twoD_plot(np.arange(50), np.arange(30), np.random.randn(50, 30))
-    fig, ax = ge.image(np.random.randn(50, 30))
+    # fig, ax = ge.image(np.random.randn(50, 30))
     
     
     ge.show()
