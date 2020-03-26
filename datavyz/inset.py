7358488686e381f6e2e52b69bfa429b437cc2f71
Yann <yann.zerlaut@icm-institute.org>
2020-03-26 11:00:01 +0100

    adding an arrow plot

--- datavyz/inset.py
+++ datavyz/inset.py
@@ -3,21 +3,25 @@ sys.path.append(str(pathlib.Path(__file__).resolve().parents[1]))
 
 from datavyz.dependencies import *
 
-def inset(graph, ax,
-          rect=[.5,.5,.5,.4],
+def inset(graph, stuff,
+          x0=0.5, y0=0.5,
+          dx=.5, dy=.5,
           facecolor='w'):
-    fig = graph.gcf()
-    box = ax.get_position()
-    width = box.width
-    height = box.height
-    inax_position  = ax.transAxes.transform(rect[0:2])
-    transFigure = fig.transFigure.inverted()
-    infig_position = transFigure.transform(inax_position)    
-    x = infig_position[0]
-    y = infig_position[1]
-    width *= rect[2]
-    height *= rect[3]  # <= Typo was here
-    subax = fig.add_axes([x,y,width,height],facecolor=facecolor)
+    if type(stuff)==mpl.figure.Figure: # if figure, no choice, if figure relative coordinates
+        subax = stuff.add_axes([x0,y0,dx,dy],facecolor=facecolor)
+    else:
+        fig = graph.gcf()
+        box = stuff.get_position()
+        width = box.width
+        height = box.height
+        inax_position  = stuff.transAxes.transform([x0,y0])
+        transFigure = fig.transFigure.inverted()
+        infig_position = transFigure.transform(inax_position)    
+        x = infig_position[0]
+        y = infig_position[1]
+        width *= dx
+        height *= dy
+        subax = fig.add_axes([x,y,width,height],facecolor=facecolor)
     return subax
 
 def add_inset(ax,
@@ -46,12 +50,12 @@ def add_inset(ax,
 
 if __name__=='__main__':
 
-    from datavyz.main import graph_env
+    from main import graph_env
     ge = graph_env('manuscript')
 
     y = np.exp(np.random.randn(100))
     fig, ax = ge.plot(y, xlabel='time', ylabel='y-value')
-    sax = ge.inset(ax, rect=[.5,.8,.5,.4])
+    sax = ge.inset(ax, x0=.5, y0=.8, dx=.5, dy=.4)
     ge.hist(y, bins=10, ax=sax, axes_args={'spines':[]}, xlabel='y-value')
     fig.savefig('docs/inset.svg')
     ge.show()
