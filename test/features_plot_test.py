7a0f16cb9457948141931b54f24a3ed7eebd7db2
Yann <yann.zerlaut@icm-institute.org>
2020-04-13 15:29:52 +0200

    update doc

--- test/features_plot_test.py
+++ test/features_plot_test.py
@@ -1,10 +1,10 @@
-import datavyz
-ge = datavyz.graph_env('screen')
+from datavyz import graph_env
 
-ge.plot(Y=np.random.randn(4, 10),
-	sY=np.random.randn(4, 10),
-        xlabel='xlabel (xunit)',
-        ylabel='ylabel (yunit)',
+ge = graph_env('screen')
+
+import numpy as np
+ge.plot(Y=np.random.randn(4, 10),sY=np.random.randn(4, 10),
+        xlabel='xlabel (xunit)', ylabel='ylabel (yunit)',
         title='datavyz demo plot')
 
 ge.show()
