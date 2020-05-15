6f9bc32477f7a5c9631c353aae0fc3024debeaa4
yzerlaut <yann.zerlaut@gmail.com>
2020-05-15 17:07:13 +0200

    up

--- test/line_plot_test.py
+++ test/line_plot_test.py
@@ -1,12 +1,11 @@
 import numpy as np
 
-from datavyz.main import graph_env
-ge = graph_env('screen')
+from datavyz import ge
 
 fig, ax = ge.plot(Y=np.random.randn(4, 10),
 	          sY=np.random.randn(4, 10),
                   xlabel='xlabel (xunit)',
                   ylabel='ylabel (yunit)',
                   title='datavyz demo plot')
-fig.savefig('docs/demo.svg')
+ge.savefig(fig, 'docs/demo.png')
 ge.show()
