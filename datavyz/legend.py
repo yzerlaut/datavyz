2bea45355fe94ec4eaf0faa1f5833a30b6175d72
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2022-01-26 15:14:24 +0100

    removed is

--- datavyz/legend.py
+++ datavyz/legend.py
@@ -109,7 +109,7 @@ def build_bar_legend(ax_cb, X, mymap,
         color_discretization = len(X)
         
     # scale : 'linear' / 'log' / 'custom'
-    if scale is 'linear':
+    if scale=='linear':
         if bounds is None:
             try:
                 bounds = [X[0]+(X[1]-X[0])/2., X[-1]+(X[1]-X[0])/2.]
@@ -117,14 +117,14 @@ def build_bar_legend(ax_cb, X, mymap,
                 bounds = [X[0], X[0]+1]
                 
         bounds = np.linspace(bounds[0], bounds[1], color_discretization)
-    elif scale is 'log10':
+    elif scale=='log10':
         if bounds is None:
             bounds = [int(np.log(X[0])/np.log(10))-.1*int(np.log(X[0])/np.log(10)),\
                       int(np.log(X[-1])/np.log(10))+1+.1*int(np.log(X[-1])/np.log(10))]
         else:
             bounds = [np.log(bounds[0])/np.log(10), np.log(bounds[1])/np.log(10)]
         bounds = np.logspace(bounds[0], bounds[1], color_discretization)
-    elif scale is 'custom':
+    elif scale=='custom':
         bounds = np.linspace(X[0]+(X[1]-X[0])/2., X[-1]+(X[1]-X[0])/2., color_discretization)
     norm = mpl.colors.BoundaryNorm(bounds, mymap.N)
     cb = mpl.colorbar.ColorbarBase(ax_cb, cmap=mymap, norm=norm,\
