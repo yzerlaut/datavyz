bf55ce1be5d4bb258a0ad06253dd342d0f04ac13
Yann Zerlaut <yann.zerlaut@gmail.com>
2022-06-09 17:36:14 +0200

    typo fontsize

--- datavyz/legend.py
+++ datavyz/legend.py
@@ -194,7 +194,7 @@ def legend(cls, ax,
     """
     """
     if fontsize is None:
-        fontsize=set_fontsize(cls, size)
+        fontsize=cls.set_fontsize(cls, size)
     if color is None:
         color = cls.default_color
 
